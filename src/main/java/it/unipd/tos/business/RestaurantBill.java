7aa1138cf024ba1c861e9367ba069720f883e4f3
Smule97 <samuelegardin1997@gmail.com>
2018-12-07 10:02:10 +0100

    Start Develop

---
+++ src/main/java/it/unipd/tos/business/RestaurantBill.java
@@ -0,0 +1,12 @@
+///////////////////////////////////////////////////////////////////
+// [Samuele] [Gardin] [1143807]
+////////////////////////////////////////////////////////////////////
+package it.unipd.tos.business;
+
+import java.util.List;
+import it.unipd.tos.model.MenuItem;
+import it.unipd.tos.business.exception.RestaurantBillException;
+
+public interface RestaurantBill {
+    double getOrderPrice(List<MenuItem> itemsOrdered) throws RestaurantBillException;
+}
---
+++ src/main/java/it/unipd/tos/business/Total.java
@@ -0,0 +1,13 @@
+///////////////////////////////////////////////////////////////////
+// [Samuele] [Gardin] [1143807]
+////////////////////////////////////////////////////////////////////
+package it.unipd.tos.business;
+
+import it.unipd.tos.business.exception.RestaurantBillException;
+import it.unipd.tos.model.MenuItem;
+
+import java.util.List;
+
+public class Total implements RestaurantBill {
+
+}
