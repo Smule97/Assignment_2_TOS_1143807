1407c8b81f352cd62c9dd26e233439353e2427e3
Smule97 <samuelegardin1997@gmail.com>
2018-12-07 10:19:30 +0100

    close #2

--- src/main/java/it/unipd/tos/business/Total.java
+++ src/main/java/it/unipd/tos/business/Total.java
@@ -6,16 +6,27 @@ package it.unipd.tos.business;
 import it.unipd.tos.business.exception.RestaurantBillException;
 import it.unipd.tos.model.MenuItem;
 
+import java.awt.*;
 import java.util.List;
 
 public class Total implements RestaurantBill {
 
     @Override
     public double getOrderPrice(List<MenuItem> itemsOrdered) throws RestaurantBillException {
-        double toReturn = 0;
+        double toReturn = 0, minPricePizza=0, contPizze=0;
+
         for (MenuItem x : itemsOrdered) {
+            if(x.getType() == "Pizze"){
+                contPizze++;
+                if(x.getPrice() < minPricePizza)
+                    minPricePizza = x.getPrice();
+            }
             toReturn += x.getPrice();
         }
+
+        if(contPizze > 10)
+            toReturn -= minPricePizza;
+
         return toReturn;
     }
 }
